1b98580d0af624e4e2a63f850d8824bfcd83a73f
Chethan Prabhu <chethanprabhu65@gmail.com>
2023-08-18 23:09:17 +0530

    fixed cors issue
    https://app.clickup.com/t/85zttmb05

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const cors = require('cors');
+
 const getAllProducts = require("../../data/getAllData.json")
 const app = express();
+app.use(cors());
 const router = express.Router();
 
 app.use('/.netlify/functions/app', router);
